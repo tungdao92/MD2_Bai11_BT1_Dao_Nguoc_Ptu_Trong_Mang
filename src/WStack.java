e9007794709428b484368eaa4b231e8b1f0cb642
unknown <tungdao923@gmail.com>
2022-08-08 21:32:07 +0700

    Dao nguoc phan tu trong mang

--- src/WStack.java
+++ src/WStack.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -7,16 +8,15 @@ public class WStack {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập vào chuỗi");
         String mWord = scanner.nextLine();
-        String [] list = mWord.split(" ");
-        mWord = "";
+        String[] list = mWord.split("");
+
         for (int i = 0; i < list.length; i++) {
             wStack.push(list[i]);
         }
+        System.out.println(Arrays.toString(list));
         for (int i = 0; i < list.length; i++) {
-            mWord+=wStack.pop();
-
+            list[i] = wStack.pop();
         }
-        System.out.println("=========="+mWord);
+        System.out.println(list);
     }
-
 }
